23e06c507eed9b13a55f79912bd988fb1689ca76
Daankovski <danielepke@hotmail.com>
2015-09-15 09:36:32 +0200

    added scrolling background and car controls

---
+++ Assets/_Scripts/carController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class carController : MonoBehaviour {
+
+	public float carSpeed = 8f;
+	public float carVert = 5f;
+	Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+		position.y += Input.GetAxis("Vertical") * carVert * Time.deltaTime;
+		transform.position = position;
+		//Input.GetAxis("Vertical");
+	}
+}
---
+++ Assets/_Scripts/roadController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class roadController : MonoBehaviour {
+
+	public float roadSpeed;
+	Vector2 offset;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offset = new Vector2 (0, Time.time * roadSpeed);
+		GetComponent<Renderer> ().material.mainTextureOffset = offset;
+	}
+}
