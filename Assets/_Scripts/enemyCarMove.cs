6c30e98c9d6b80a0928a9cd8049504416b7ae636
Phineas Moore <phineasmoore@gmail.com>
2015-09-16 08:47:56 +0200

    random car spawn

---
+++ Assets/_Scripts/enemyCarMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyCarMove : MonoBehaviour {
+
+	public int speed; 
+
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range(5,10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3 (0, 1, 0) * speed * Time.deltaTime);
+	}
+}
