caab6e527821049284c9e4c20ebe998cf6ca1fec
Daankovski <danielepke@hotmail.com>
2015-09-23 22:02:07 +0200

    Commit: Fixed pausing the score

---
+++ Assets/_Scripts/PlayerInputs.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInputs : MonoBehaviour {
+
+	public int playerNumber;
+	
+	public float x{
+		get{
+			return Input.GetAxis(string.Format("Player{0}X",playerNumber));
+		}
+	}
+
+	public float y{
+		get{
+			return Input.GetAxis(string.Format("Player{0}Y", playerNumber));
+		}
+	}
+
+
+}
